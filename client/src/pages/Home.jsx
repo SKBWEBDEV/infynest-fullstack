5a602ac97e005da8f2287f7d125b1017e88c10a4
SKBWEBDEV <najmujshakib025@gmail.com>
2026-08-10 14:22:45 +0600

    fix hoice catagoris ta

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,26 +1,23 @@
+
 import React from "react";
 
 import HeroSlider from "../components/home/HeroSlider";
 import CategoryProducts from "../components/home/Categories";
-import NewArrivals from "../components/home/NewArrivals";
-import SaleProducts from "../components/home/SaleProducts";
-import PromoBanners from "../components/home/PromoBanners";
 
 export default function Home() {
   return (
-    <div className="space-y-10 pb-20 overflow-hidden">
-
+    <>
       {/* Hero */}
       <HeroSlider />
 
-      {/* Spider-Man */}
+      {/* Regular Fit */}
       <CategoryProducts
         title="Regular Fit"
-        categorySlug="Regular Fit"
+        categorySlug="spider-man"
         badge="HOT"
       />
 
-      {/* Chainsaw Man */}
+      {/* Drop Shoulder */}
       <CategoryProducts
         title="Drop Shoulder"
         categorySlug="chainsaw-man"
@@ -34,34 +31,13 @@ export default function Home() {
         badge="NEW"
       />
 
-      {/* Ghost Rider */}
-      {/* <CategoryProducts
-        title="Ghost Rider"
-        categorySlug="ghost-rider"
-        badge="HOT SALE"
-      /> */}
-
       {/* Essentials */}
       <CategoryProducts
         title="Essentials"
         categorySlug="essentials"
         badge="HOT SALE"
       />
-
-      {/* Anime */}
-      {/* <CategoryProducts
-        title="Anime"
-        categorySlug="anime"
-        badge="HOT SALE"
-      /> */}
-
-      {/* Venom */}
-      {/* <CategoryProducts
-        title="Venom"
-        categorySlug="venom"
-        badge="HOT SALE"
-      /> */}
-
-    </div>
+    </>
   );
 }
+
